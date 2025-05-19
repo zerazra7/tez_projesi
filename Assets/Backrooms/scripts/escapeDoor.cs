54a65513f67c375cdf0358f4f29ff082c6789266
zehra giray <zgiray21@posta.pau.edu.tr>
2025-05-19 22:06:18 +0300

    ESCAPE SOUND EKLEND

--- Assets/Backrooms/scripts/escapeDoor.cs
+++ Assets/Backrooms/scripts/escapeDoor.cs
@@ -2,39 +2,24 @@ using UnityEngine;
 
 public class EscapeDoor : MonoBehaviour
 {
-    bool showMessage = false;
-    float timer = 0f;
+    public AudioClip escapeSound; // Inspector'dan vereceðiz
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = gameObject.AddComponent<AudioSource>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") && PlayerPrefs.GetInt("HasKeycard", 0) == 1)
         {
+            if (escapeSound != null && !audioSource.isPlaying)
+            {
+                audioSource.PlayOneShot(escapeSound);
+            }
+
             EndGameManager.Instance.ShowEndGame();
-        }
-    }
-
-    private void Update()
-    {
-        if (showMessage)
-        {
-            timer -= Time.deltaTime;
-            if (timer <= 0f)
-                showMessage = false;
-        }
-    }
-
-    private void OnGUI()
-    {
-        if (showMessage)
-        {
-            GUIStyle style = new GUIStyle(GUI.skin.label);
-            style.alignment = TextAnchor.MiddleCenter;
-            style.fontSize = 35;
-            style.fontStyle = FontStyle.Bold;
-            style.normal.textColor = Color.black;
-
-            Rect full = new Rect(0, 0, Screen.width, Screen.height);
-            GUI.Label(full, "Tebrikler, kurtuldunuz!", style);
         }
     }
 }
